b7a134217fd42563fd97beda356c246011f8d773
Fourth1755 <fourthe.hero@gmail.com>
2022-01-25 20:29:48 +0700

    Update OurGroup.java

--- OurGroup.java
+++ OurGroup.java
@@ -6,7 +6,12 @@ public class OurGroup {
     public OurGroup()
     {
         groupMembers = new Vector<String>();
+<<<<<<< HEAD
         groupMembers.add("Pooriwat"); //PUT YOUR NAME HERE
+=======
+        groupMembers.add("Thanapat Wongtungjaroensuk");
+	groupMembers.add("Nuttawat Juntawong"); //PUT YOUR NAME HERE
+>>>>>>> parent of 949a5d2 (My first group changes to OurGroup.java)
     }
     public Vector<String> getGroupMembers () {
         return groupMembers;
